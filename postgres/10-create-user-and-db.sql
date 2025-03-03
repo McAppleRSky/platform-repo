640f490e056c559ca1cb6806b19e7b9264091abc
McAppleRSky <mcapple@yandex.ru>
2025-03-03 19:15:09 +0300

    clean

--- postgres/10-create-user-and-db.sql
+++ postgres/10-create-user-and-db.sql
@@ -1,7 +1,2 @@
--- file: 10-create-user-and-db.sql
--- CREATE ROLE program WITH PASSWORD 'test';
--- ALTER ROLE program WITH LOGIN;
--- CREATE DATABASE homework3;
--- GRANT ALL PRIVILEGES ON DATABASE homework3 TO program;
 CREATE SCHEMA wdata;
 SET search_path TO wdata, public;
